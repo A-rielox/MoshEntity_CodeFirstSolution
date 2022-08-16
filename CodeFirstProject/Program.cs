01a5912abd3b21d62d1cbb4f17cd9bb9bb0a6f40
A-rielox <arielox.ag@gmail.com>
2022-08-16 17:30:46 -0400

    Add project files.

---
+++ CodeFirstProject/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstProject
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public CourseLevel Level { get; set; }
+        public float FullPrice { get; set; }
+        public Author Author { get; set; }
+        public IList<Tag> Tags { get; set; }
+    }
+
+    public class Author
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public IList<Course> Courses { get; set; }
+    }
+
+    public class Tag
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public IList<Course> Courses { get; set; }
+    }
+
+    public enum CourseLevel
+    {
+        Beginner = 1,
+        Intermediate = 2,
+        Advanced = 3
+    }
+
+    public class PlutoContext : DbContext
+    {
+        // un DbSet es una collection of object that represents a table in the DB
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
+
+//instalar EF install-package EntityFramework -Version:6.1.3
